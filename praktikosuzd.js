10f22a8d6868578154dee6296fc7d6b0a7dcfd29
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-04-04 14:29:13 +0300

    commitas

--- praktikosuzd.js
+++ praktikosuzd.js
@@ -61,3 +61,134 @@ const atvirukoBalses=['a','e','o','u','i'];
 for(let i=0; i<atvirukoBalses.length;i++){
   console.log('Raide'+ atvirukoBalses[i]+'pasikartoja'+ atvirukoLipdukai(atvirukoBalses[i]))
 }
+
+
+//sukurti sarasa
+
+let miestai11=[
+  {miestas:('Kaunas', 89.6)},
+  {miestas:('Klaipėda', 307.3)},
+  {miestas:('Šiauliai', 213)},
+  {miestas:('Panvėžys', 136)},
+  {miestas:('Molėtai', 65)},
+  {miestas:('Anykščiai', 114)},
+  {miestas:('Prienai', 97)},
+  {miestas:('Varėna', 83)},
+  {miestas:('Marijampolė', 162)},
+  {miestas:('Jonava', 92)},
+  {miestas:('Vilnius', 0)}]
+  //a
+  function kurTolimiausiai (miestai, greitis, laikas)
+  {
+      let maxA = 0;
+      let kelias = greitis * laikas;
+      let maxM = 0;
+  
+      for(let i = 0; i<miestai.length; i+=2)
+      {
+          //console.log(miestai[i]);
+          if(miestai[i+1]<= kelias && miestai[i+1]> maxA)
+          {
+              maxA = miestai[i+1];
+              maxM = i;
+          }
+      }
+      console.log(miestai[maxM] + ' ' + miestai[maxM+1]);
+  
+  }
+  
+  kurTolimiausiai(miestai11,80,2)
+  
+  
+  
+  function ArSpetume (miestai, miestas, laikas) //s = v*t 
+  {
+  
+      for(let i = 0; i<miestai.length; i+=2)
+      {
+          if(miestai[i]===miestas)
+          {
+              if(miestai[i+1]/laikas <= 120)
+              {
+                  console.log("spesim " + miestai[i+1]/laikas)
+              }
+          }
+          else
+          {
+              console.log("miesto neradau")
+          }
+      }
+  }
+  
+  ArSpetume(miestai11, 'marijampolė', 2)
+  
+  
+  function ArUzteks (miestai, pinigai, Kkaina, deginam)
+  {
+      let kuras = 30 + pinigai/Kkaina;
+      let atstumas = kuras / deginam;
+      let maxA = 0
+      let maxM = 0
+      for(let i = 0; i<miestai.length; i+=2)
+          {
+              //console.log(miestai[i]);
+              if(miestai[i+1]<= atstumas && miestai[i+1]> maxA)
+              {
+                  maxA = miestai[i+1];
+                  maxM = i;
+              }
+          }
+  
+      console.log(miestai[maxM] + ' ' + maxA)
+  
+  }
+  
+  ArUzteks(miestai11, 10, 0.5, 0.5)
+  
+  
+  const KelioSalygos = ['geras', 'prastas', 'taisomas', 'prastas'];
+  
+  function realusLaikas (miestai, salyga)
+  {
+  for(let i = 0; i<miestai.length; i+=2)
+      {
+          if(salyga[i/2]==='geras')
+          {
+              console.log(miestai[i+1])
+          }
+          if(salyga[i/2]==='prastas')
+              {
+                  console.log(miestai[i+1]*1.5)
+              }
+              if(salyga[i/2]==='taisomas')
+                  {
+                      console.log(miestai[i+1]*2)
+                  }
+      }
+  }
+  
+  realusLaikas(miestai11, KelioSalygos)
+  
+  
+  // parasyti funkcija,kuri apskaiciuotu kiek sveikuju,teigiamu skaiciu yra tam tikru skaiciu ir isvestu rezultata grafiskai
+  let sarasas=[1,1,2,3,3,3,4];
+  let skKiekis=0;
+  function skaiciuoti(sarasas){
+      for(let i=0;i<sarasas.length;i++){
+          if(sarasas[i]>0){
+              skKiekis++;
+          }
+      }
+      console.log(`sveikuju skaiciu yra ${skKiekis}`)
+      for(let i=0;i<skKiekis;i++){
+          console.log('*')
+      }
+  }
+  
+  
+  
+  
+  
+  
+  
+
