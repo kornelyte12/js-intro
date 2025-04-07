8228eda84901761fd81aadf16033126e719d5ead
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-04-07 08:57:47 +0300

    array

--- js/13-array-methods.js
+++ js/13-array-methods.js
@@ -64,3 +64,31 @@ for (let i = 0; i < n2.length; i++) {
 }
 
 console.log(n12);
+
+
+const numbers2=[10,2,8,4,6];
+console.log(numbers2);
+numbers2.push(9);//ideda nari i masyva
+console.log(numbers2);
+numbers2.pop();// salina paskutini nari
+console.log(numbers2);
+
+numbers2.unshift(1);// ideda nari i masyvo pradzia
+console.log(numbers2);
+
+numbers2.shift();// salina pirma nari
+console.log(numbers2);
+
+const magic=[1,2,3,4,5,10,11,12];
+console.log(magic.includes(10));//true ar egzistuoja masyve
+console.log(magic.includes(100));//false
+
+console.log(magic.indexOf(10));// 5 grazina indeksa []
+console.log(magic.indexOf(100));// -1 grazina -1 jei neranda []
+
+const texts=['labas','rytas','pasauli'];
+const zodziai=`reikalingi zodziai: ${texts.join[', ']}`;// sujungia masyvo elementus i viena stringa
+console.log(texts.join(' '));// sujungia masyvo elementus i viena stringa
+console.log(zodziai);
+
+
