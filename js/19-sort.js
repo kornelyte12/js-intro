c815dff70f355d8bbd03adfe093f1cfde809b743
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-04-09 09:27:07 +0300

    sort

---
+++ js/19-sort.js
@@ -0,0 +1,16 @@
+//sort
+
+//tekstai sort(abeceles tvarka pradeda nuo didziuju raidziu)
+
+const text=['labas','rytas','Lietuva','duona','aviza','Zirnis','anakonda'];
+console.log(text);
+sort.text();
+console.log(sort.text);
+const pseud=['d','b','c','a'];
+console.log(pseud);
+
+//skaiciai (prasideda nuo 1 ( atsakymas 10,2,4,6,8))
+const numbers=[10,2,8,4,6].sort((a,b)=>a-b);//jei pakeisi b-a bus atbuline tvarka
+console.log(numbers);
+
+
