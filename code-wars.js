f81228e17b994f171f9adfcff019b7fe63820155
Kornelija Večkienė <kornelija.acisauskaite@gmail.com>
2025-04-09 11:42:55 +0300

    21

--- code-wars.js
+++ code-wars.js
@@ -1,7 +1,9 @@
 let arr=[1,-4,7,12]
 let sum=0
+function sumaTeigiamu(arr){
 for(let i=0;i<arr.length;i++){
-    if(i>0){
-        return [i]
+    if([i]>0){
+        return sum+=i
     }
-}
+}}
+console.log(sumaTeigiamu(arr));
